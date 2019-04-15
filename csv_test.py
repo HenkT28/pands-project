f5f3757f7c15e124a5bc93e92429fbb63c4f4cd2
Henk Tjalsma <G00376321@gmit.ie>
2019-04-15 21:06:24 +0100

    Added script using pandas package, 150419:21:06

---
+++ csv_test.py
@@ -0,0 +1,16 @@
+# https://www.shanelynn.ie/summarising-aggregation-and-grouping-data-in-python-pandas/
+
+# Load the Pandas libraries with alias 'pd'
+import pandas as pd
+
+# Load data from csv file - in the same directory that your python process is based
+# Control delimiters, rows, column names with read_csv 
+
+# https://data36.com/pandas-tutorial-1-basics-reading-data-files-dataframes-data-selection/
+
+# https://www.geeksforgeeks.org/different-ways-to-import-csv-file-in-pandas/
+
+# making data frame   
+df = pd.read_csv('iris.csv')   
+    
+print(df.head())     
