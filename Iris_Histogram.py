14ef09f838dd443bac3e41fd05548de0f518fdf8
Henk Tjalsma <G00376321@gmit.ie>
2019-04-24 18:26:40 +0100

    Added Petal_Sepal_dist script - using matplotlib, 240419:18:26

--- Iris_Histogram.py
+++ Iris_Histogram.py
@@ -18,10 +18,12 @@ iris= load_iris()
 colors= ['blue', 'red', 'green']
 
 # https://github.com/PyCQA/pylint/issues/1161 - hitting error -> Instance of 'tuple' has no 'data' memberpylint(no-member), but script works ok. 
-for feature in range(iris.data.shape[1]): # (shape = 150, 4)
-    plt.subplot(2, 2, feature+1) # subplot starts from 1 (not 0)
+# (shape = 150, 4)
+for feature in range(iris.data.shape[1]): 
+# Subplot starts from 1 (not 0)
+    plt.subplot(2, 2, feature+1) 
     for label, color in zip(range(len(iris.target_names)), colors):
-        # find the label and plot the corresponding data
+# Find the label and plot the corresponding data
         plt.hist(iris.data[iris.target==label, feature],
                  label=iris.target_names[label],
                  color=color)
---
+++ Petal_Sepal_dist.py
@@ -0,0 +1,40 @@
+# Henk Tjalsma, 2019
+# Iris Data Set - plotting iris data set
+
+# https://www.kaggle.com/abhishekkrg/python-iris-data-visualization-and-explanation
+# https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
+# Importing Package 
+import numpy
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Reading Iris Dataset in Pandas Dataframe
+data = pandas.read_csv("iris_original.csv")
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
+
+setosa=data[data['species']=='setosa']
+versicolor =data[data['species']=='versicolor']
+virginica =data[data['species']=='virginica']
+
+# https://stackoverflow.com/questions/38666527/what-is-the-necessity-of-plt-figure-in-matplotlib
+plt.figure()
+# Adding multiple Axes objects  
+fig,ax=plt.subplots(1,2,figsize=(18, 10))
+
+setosa.plot(x="sepal_length", y="sepal_width", kind="scatter",ax=ax[0],label='setosa',color='r')
+versicolor.plot(x="sepal_length",y="sepal_width",kind="scatter",ax=ax[0],label='versicolor',color='b')
+virginica.plot(x="sepal_length", y="sepal_width", kind="scatter", ax=ax[0], label='virginica', color='g')
+
+setosa.plot(x="petal_length", y="petal_width", kind="scatter",ax=ax[1],label='setosa',color='r')
+versicolor.plot(x="petal_length",y="petal_width",kind="scatter",ax=ax[1],label='versicolor',color='b')
+virginica.plot(x="petal_length", y="petal_width", kind="scatter", ax=ax[1], label='virginica', color='g')
+
+ax[0].set(title='Sepal comparasion ', ylabel='sepal-width')
+ax[1].set(title='Petal Comparasion',  ylabel='petal-width')
+ax[0].legend()
+ax[1].legend()
+plt.show()
+# plt.close()
