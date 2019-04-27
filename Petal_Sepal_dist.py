7769ccbcb5fa883292f1ab26532ee4d55010db18
Henk Tjalsma <G00376321@gmit.ie>
2019-04-27 23:52:37 +0100

    Changed Petal_Sepal_dist.py - added comments, 270419:23:52

--- Petal_Sepal_dist.py
+++ Petal_Sepal_dist.py
@@ -1,5 +1,5 @@
 # Henk Tjalsma, 2019
-# Iris Data Set - plotting iris data set
+# Iris Data Set - petal and sepal distibution
 
 # https://www.kaggle.com/abhishekkrg/python-iris-data-visualization-and-explanation
 # https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # Reading Iris Dataset in Pandas Dataframe
 data = pandas.read_csv("iris_original.csv")
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
+names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
 setosa=data[data['species']=='setosa']
 versicolor =data[data['species']=='versicolor']
@@ -31,6 +31,7 @@ setosa.plot(x="petal_length", y="petal_width", kind="scatter",ax=ax[1],label='se
 versicolor.plot(x="petal_length",y="petal_width",kind="scatter",ax=ax[1],label='versicolor',color='b')
 virginica.plot(x="petal_length", y="petal_width", kind="scatter", ax=ax[1], label='virginica', color='g')
 
+# Setting title of the graphs
 ax[0].set(title='Sepal Comparison', ylabel='sepal-width')
 ax[1].set(title='Petal Comparison',  ylabel='petal-width')
 ax[0].legend()
