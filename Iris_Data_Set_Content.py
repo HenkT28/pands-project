b1e524059d1338a1948d13a9ccf58b3f21848da2
Henk Tjalsma <G00376321@gmit.ie>
2019-04-27 22:59:06 +0100

    Changed Iris_Data_Set_Content.py, 270419:22:58

--- Iris_Data_Set_Content.py
+++ Iris_Data_Set_Content.py
@@ -1,5 +1,5 @@
 # Henk Tjalsma, 2019
-# Iris Data Set - plotting iris data set
+# Iris Data Set - content
 
 # https://www.kaggle.com/abhishekkrg/python-iris-data-visualization-and-explanation
 # https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
@@ -10,10 +10,10 @@ import pandas
 
 # Reading Iris Dataset in Pandas Dataframe
 data = pandas.read_csv("iris_original.csv")
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
+names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
 # Printing DataFrame - only first 30 rows to understand what data look like
-# Data has 5 Columns - first four are features and fifth is Classfication of the Iris type
+# Data has 5 Columns - first four are features and fifth is classfication
 # print(data.head(30))
 
 # https://stackoverflow.com/a/52281061 -> select first 3 Rows With Distinct Values in Specific Column "species"
